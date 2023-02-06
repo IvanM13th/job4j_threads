6000bf008c70202cf61b412f0d7c75c8a230fd9f
IvanM13th <bmchek13@gmail.com>
2023-02-06 11:52:14 +0500

    8. Скачивание файла с ограничением. [#144271 #388774]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -6,21 +6,27 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.time.Duration;
+import java.time.Instant;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String saveDir;
+    private static final int SLEEP_TIME = 1000;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String saveDir) {
         this.url = url;
         this.speed = speed;
+        this.saveDir = saveDir;
     }
 
     public static void main(String[] args) throws InterruptedException {
         validate(args);
         String url = args[0];
+        String saveDir = getFileName(args[0]);
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        Thread wget = new Thread(new Wget(url, speed, saveDir));
         wget.start();
         wget.join();
     }
@@ -28,12 +34,19 @@ public class Wget implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream out = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream out = new FileOutputStream(saveDir)) {
             byte[] dataBuffer = new byte[speed];
             int bytesRead;
+            int downloadedData = 0;
+            Instant start = Instant.now();
             while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
                 out.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(1000);
+                downloadedData += bytesRead;
+                if (downloadedData == speed && Duration.between(start, Instant.now()).toMillis() < SLEEP_TIME) {
+                    Thread.sleep(SLEEP_TIME);
+                    downloadedData = 0;
+                    start = Instant.now();
+                }
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -52,5 +65,10 @@ public class Wget implements Runnable {
         if (Integer.parseInt(args[1]) == 0) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static String getFileName(String file) {
+        String[] n = file.split("/");
+        return n[n.length - 1];
     }
 }
