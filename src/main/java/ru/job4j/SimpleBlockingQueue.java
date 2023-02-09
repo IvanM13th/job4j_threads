53387323872c80542a1873bd32646f2413e7f9db
IvanM13th <bmchek13@gmail.com>
2023-02-09 08:46:41 +0500

    1. Реализовать шаблон Producer Consumer. [#1098 #389547]

--- src/main/java/ru/job4j/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -21,8 +21,8 @@ public class SimpleBlockingQueue<T> {
         while (queue.size() == limit) {
             this.wait();
         }
-        notifyAll();
         queue.add(value);
+        notifyAll();
     }
 
     public synchronized T poll() throws InterruptedException {
